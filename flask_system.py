437e809169e52dd87a60bc1557fa721dd074e60f
Max Woolf <max@minimaxir.com>
2016-10-18 21:16:53 -0700

    PEP8 Compliance

--- flask_system.py
+++ flask_system.py
@@ -1,10 +1,11 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_cors import cross_origin
 import json
+import logging
 import os
-import psutil
 import time
+
+import psutil
+from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)   # Necessary since API is running locally
@@ -12,9 +13,9 @@ CORS(app)   # Necessary since API is running locally
 period = 1
 
 # Disable Flask console messages: http://stackoverflow.com/a/18379764
-import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
 
 @cross_origin()
 @app.route('/')
@@ -22,7 +23,7 @@ def index():
 
     disk_write_data_start = psutil.disk_io_counters(perdisk=False)
     io_data_start = psutil.net_io_counters()
-    
+
     time.sleep(period)
 
     cpu_data = psutil.cpu_percent(interval=None)
@@ -34,30 +35,30 @@ def index():
     data = {
         'cpu': {
             'percent': cpu_data
-            },
+        },
         'ram': {
             'percent': ram_data[2],
             'total': ram_data[0],
             'used': ram_data[3]
-            },
+        },
         'disk': {
             'total': disk_data[0],
             'used': disk_data[1],
             'percent': disk_data[3],
-            'read_bytes_sec': (disk_write_data[2] - disk_write_data_start[2]) \
-                                / period,
-            'write_bytes_sec': (disk_write_data[3] - disk_write_data_start[3]) \
-                                / period
-            },
+            'read_bytes_sec': (disk_write_data[2] - disk_write_data_start[2])
+            / period,
+            'write_bytes_sec': (disk_write_data[3] - disk_write_data_start[3])
+            / period
+        },
         'io': {
             'sent_bytes_sec': (io_data[0] - io_data_start[0]) / period,
             'received_bytes_sec': (io_data[1] - io_data_start[1]) / period
-            }
+        }
     }
 
     return json.dumps(data)
 
 if __name__ == "__main__":
-    print "System API up at http://localhost:5002"
+    print("System API up at http://localhost:5002")
     PORT = int(os.getenv('PORT', 5002))
     app.run(port=PORT, threaded=True)
